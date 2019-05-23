04fb8a0e72e69c014e6542e23576d658d4853252
sam_arbid <samarbid13@users.noreply.github.com>
2019-05-23 23:32:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+
+// REGEX
+/* let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; // Change this line
 let result = waldoRegex.test(waldoIsHiding);
-console.log(result)
+console.log(result) */
+// ---------
+
+// Match a Literal String with Different Possibilities
+
+
+/* Using regexes like /coding/, you can look for the pattern "coding" in another string.
+
+This is powerful to search single strings, but it's limited to only one pattern. You can search for multiple patterns using the alternation or OR operator: |.
+
+This operator matches patterns either before or after it. For example, if you wanted to match "yes" or "no", the regex you want is /yes|no/.
+
+You can also search for more than just two patterns. You can do this by adding more patterns with more OR operators separating them, like /yes|no|maybe/. */
+
+
+let petString = "James has a pet cat.";
+let petRegex = /change/; // Change this line
+let result = petRegex.test(petString);
+console.log(result);
+
