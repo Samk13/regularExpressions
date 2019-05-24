3d7f108823d7e49dbdf775356f038f758fa83133
sam_arbid <samarbid13@users.noreply.github.com>
2019-05-24 14:43:31 +0200

    regex rouls

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 let waldoRegex = /Waldo/; // Change this line
 let result = waldoRegex.test(waldoIsHiding);
 console.log(result) */
-// ---------
+// ***************
 
 // Match a Literal String with Different Possibilities
 
@@ -15,11 +15,354 @@ This is powerful to search single strings, but it's limited to only one pattern.
 
 This operator matches patterns either before or after it. For example, if you wanted to match "yes" or "no", the regex you want is /yes|no/.
 
-You can also search for more than just two patterns. You can do this by adding more patterns with more OR operators separating them, like /yes|no|maybe/. */
+You can also search for more than just two patterns. You can do this by adding more patterns with more OR operators separating them, like /yes|no|maybe/. 
+Complete the regex petRegex to match the pets "dog", "cat", "bird", or "fish".
 
+//--------------
 
 let petString = "James has a pet cat.";
-let petRegex = /change/; // Change this line
+let petRegex = /dog|cat|bird|fish/; // Change this line
 let result = petRegex.test(petString);
+console.log(result); */
+
+
+
+
+//***************** */
+
+
+
+//You can match both cases using what is called a flag. There are other flags but here you'll focus on the flag that ignores case - the i
+/* 
+let myString = "freeCodeCamp";
+let fccRegex = /FREECodeCamp/i; // Change this line
+let result = fccRegex.test(myString);
+console.log(result); */
+
+
+
+// ********************
+
+
+
+// You can also extract the actual matches you found with the .match() method.
+
+/* let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
+console.log(result); */
+
+
+
+//********************* 
+
+
+// To search or extract a pattern more than once, you can use the g flag.
+
+/* 
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/gi; // Change this line
+// let result = twinkleStar.match(starRegex); // Change this line
+// console.log(result); 
+*/
+
+//********************* 
+/* 
+you can save time using the wildcard character: .
+The wildcard character . will match any one character. The wildcard is also called dot and period. You can use the wildcard character just like any other character in the regex. For example, if you wanted to match "hug", "huh", "hut", and "hum", you can use the regex /hu./ to match all four words. */
+
+/* let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /et./; // Change this line
+let result = unRegex.test(exampleStr);
 console.log(result);
+
+ */
+
+ // **********************
+
+
+
+/* 
+ ou can search for a literal pattern with some flexibility with character classes. Character classes allow you to define a group of characters you wish to match by placing them inside square ([ and ]) brackets.
+
+For example, you want to match "bag", "big", and "bug" but not "bog". You can create the regex /b[aiu]g/ to do this. The [aiu] is the character class that will only match the characters "a", "i", or "u". */
+
+/* let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[ab]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+console.log(result) */
+
+
+
+//***********************
+
+
+
+
+/* 
+Inside a character set, you can define a range of characters to match using a hyphen character: -.
+
+//For example, to match lowercase letters a through e you would use [a-e]. */
+
+
+/* let quoteSample = "The Quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/ig; // Change this line
+let result = quoteSample.match(alphabetRegex); // Change this line
+console.log(result); */
+
+
+
+//**************
+
+
+// For example, /[0-5]/ matches any number between 0 and 5, including the 0 and 5.
+
+ // Also, it is possible to combine a range of letters and numbers in a single character set.
+
+
+// let quoteSample = "Blueberry 3.141592653s are delicious.";
+// let myRegex = /[h-s2-6]/ig; // Change this line
+// let result = quoteSample.match(myRegex); // Change this line
+// console.log(result);
+
+
+// **************************
+
+
+// To create a negated character set, you place a caret character (^) after the opening bracket and before the characters you do not want to match.
+
+// For example, /[^aeiou]/gi matches all characters that are not a vowel. Note that characters like ., !, [, @, / and white space are matched - the negated vowel character set only excludes the vowel characters.
+
+
+// let quoteSample = "3 blind @ {{mice.";
+// let myRegex = /[^aeiou0-9]/ig; // Change this line
+// let result = quoteSample.match(myRegex); // Change this line
+// console.log(result);
+
+
+//  ********************
+
+
+/*
+Match Characters that Occur One or More Times
+You can use the + character to check if that is the case. Remember, the character or pattern has to be present consecutively. That is, the character has to repeat one after the other.
+//For example, /a+/g would find one match in "abc" and return ["a"]. Because of the +, it would also find a single match in "aabc" and return ["aa"].
+*/ 
+
+// let difficultSpelling = "Mississippi";
+// let myRegex = /s+/ig; // Change this line
+// let result = difficultSpelling.match(myRegex);
+
+// console.log(result);
+
+
+
+
+
+
+// ************************
+
+
+
+
+// an option that matches characters that occur zero or more times.
+
+// The character to do this is the asterisk or star: *.
+
+// let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+// let chewieRegex = /Aa*/; // Change this line
+// let result = chewieQuote.match(chewieRegex);
+// console.log(result);
+
+
+
+// *******************
+
+/*
+Find Characters with Lazy Matching
+In regular expressions, a greedy match finds the longest possible part of a string that fits the regex pattern and returns it as a match. The alternative is called a lazy match, which finds the smallest possible part of the string that satisfies the regex pattern.
+
+You can apply the regex /t[a-z]*i/ to the string "titanic". This regex is basically a pattern that starts with t, ends with i, and has some letters in between.
+
+Regular expressions are by default greedy, so the match would return ["titani"]. It finds the largest sub-string possible to fit the pattern.
+
+However, you can use the ? character to change it to lazy matching. "titanic" matched against the adjusted regex of /t[a-z]*?i/ returns ["ti"].
+
+
+
+*/ 
+
+// let text = "<h1>Winter is coming</h1>";
+// let myRegex = /<.*?>/ig; // Change this line
+// let result = text.match(myRegex);
+
+// console.log(result);
+
+
+
+
+
+// example crowd gathering
+// let crowd = 'P1P2P3P4P5P6CCCP7P8P9P2P1P5P4CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCP3';
+
+// let reCriminals = /C+/g; // Change this line
+
+// let matchedCriminals = crowd.match(reCriminals);
+// console.log(matchedCriminals);
+
+
+
+
+//  ***********************
+
+
+// Match Beginning String Patterns
+// Prior challenges showed that regular expressions can be used to look for a number of matches. They are also used to search for patterns in specific positions in strings.
+
+// In an earlier challenge, you used the caret character (^) inside a character set to create a negated character set in the form [^thingsThatWillNotBeMatched]. Outside of a character set, the caret is used to search for patterns at the beginning of strings.
+// string start:
+
+// let rickyAndCal = "Cal and Ricky both Cal like racing.";
+// let calRegex = /^cal/ig; // Change this line
+// let result = calRegex.test(rickyAndCal);
+// let result2 = rickyAndCal.match(calRegex);
+// console.log(result);
+// console.log(result2);
+
+
+
+//   *******************
+
+
+
+
+// Match Ending String Patterns
+// In the last challenge, you learned to use the caret character to search for patterns at the beginning of strings. There is also a way to search for patterns at the end of strings.
+
+// You can search the end of strings using the dollar sign character $ at the end of the regex.
+
+// let caboose = "The last car on a train is the caboose";
+// let lastRegex = /caboose$/; // Change this line
+// let result = lastRegex.test(caboose);
+// console.log(result);
+
+
+// ************************
+
+
+
+
+
+
+
+
+
+// Match All Letters and Numbers
+// Using character classes, you were able to search for all letters of the alphabet with [a-z]. This kind of character class is common enough that there is a shortcut for it, although it includes a few extra characters as well.
+
+// The closest character class in JavaScript to match the alphabet is \w. This shortcut is equal to [A-Za-z0-9_]. This character class matches upper and lowercase letters plus numbers. Note, this character class also includes the underscore character (_).
+
+// let longHand = /[A-Za-z0-9_]+/;
+// let shortHand = /\w+/;
+// let numbers = "42";
+// let varNames = "important_var";
+// longHand.test(numbers); // Returns true
+// shortHand.test(numbers); // Returns true
+// longHand.test(varNames); // Returns true
+// shortHand.test(varNames); // Returns true
+// These shortcut character classes are also known as shorthand character classes.
+
+
+// Use the shorthand character class \w to count the number of alphanumeric characters in various quotes and strings.
+
+
+// let quoteSample = "How vexingly qu123 456 7890 ABC def GHI jkl MNO pqr STU vwx YZ.";
+// let alphabetRegexV2 = /\w/g; // Change this line
+// let result = quoteSample.match(alphabetRegexV2);
+// console.log(result.length +"\n" + result);
+
+
+
+//  *************************
+
+
+
+
+
+// Match Everything But Letters and Numbers
+// You've learned that you can use a shortcut to match alphanumerics [A-Za-z0-9_] using \w. A natural pattern you might want to search for is the opposite of alphanumerics.
+
+// You can search for the opposite of the \w with \W. Note, the opposite pattern uses a capital letter. This shortcut is the same as [^A-Za-z0-9_].
+
+// let shortHand = /\W/;
+// let numbers = "42%";
+// let sentence = "Coding!";
+// numbers.match(shortHand); // Returns ["%"]
+// sentence.match(shortHand); // Returns ["!"]
+
+// Use the shorthand character class \W to count the number of non-alphanumeric characters in various quotes and strings.
+
+
+
+// let quoteSample = "The five boxing wizards jump quickly.//.,[]{}@£dfdsdfg";
+// let nonAlphabetRegex = /\W/g; // Change this line
+// let result = quoteSample.match(nonAlphabetRegex);
+// console.log(result.length +"\n" + result);
+
+
+
+//  ******************************
+
+
+
+// Match All Numbers
+// You've learned shortcuts for common string patterns like alphanumerics. Another common pattern is looking for just digits or numbers.
+
+// The shortcut to look for digit characters is \d, with a lowercase d. This is equal to the character class [0-9], which looks for a single character of any number between zero and nine.
+
+// let numString = "Your sandwich will be $5.00";
+// let numRegex = /\d/g; // Change this line
+// let result = numString.match(numRegex).length;
+// console.log(result);
+
+
+
+
+// *****************************
+
+
+// Match All Non-Numbers
+// The last challenge showed how to search for digits using the shortcut \d with a lowercase d. You can also search for non-digits using a similar shortcut that uses an uppercase D instead.
+
+// The shortcut to look for non-digit characters is \D. This is equal to the character class [^0-9], which looks for a single character that is not a number between zero and nine.
+
+
+
+// let numString = "Your sandwich will be $5.00";
+// let noNumRegex = /\D/g; // Change this line
+// let result = numString.match(noNumRegex).length;
+// console.log(result);
+
+// or 
+
+
+// let username = "JackOfAllTrades";
+// let userCheck = /[\w{2,}$\d+?]\D/g; // Change this line
+// let result = userCheck.test(username);
+// console.log(result);
+
+
+
+
+// ***************************
+
+// great website to test your regex
+// https://regex101.com/
+
+
+
+// ****************************
+
+
+
 
